2dcf74dcd7217500385cdd8e5ac296fe319b0fca
Ivan Bragin <aywan@aywan.ru>
2022-11-17 21:08:35 +0300

    logged print

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include<stdarg.h>
 
 static const char FILE_NAME[] = ".timenator";
+
+struct timespec *ct;
 
 /**
  * Printing tool usage instruction.
@@ -11,6 +14,24 @@ static const char FILE_NAME[] = ".timenator";
 void print_usage()
 {
     printf("Usage: timenator (start|stop)\n");
+}
+
+void print_log(const char* format, ...)
+{
+    time_t now;
+    time(&now);
+
+    char time_str[sizeof "2011-10-08T07:07:09Z"];
+    strftime(time_str, sizeof time_str, "%Y-%m-%dT%H:%M:%SZ", gmtime(&now));
+
+    va_list arg;
+    va_start(arg, format);
+
+    printf("[%s] ", time_str);
+    vprintf(format, arg);
+    puts("");
+
+    va_end(arg);
 }
 
 /**
@@ -64,31 +85,63 @@ void clear_storage()
  */
 void print_time(const struct timespec *t, char *e)
 {
-    printf("event=\"%s\" time=%ld.%09ld\n", e, t->tv_sec, t->tv_nsec);
+    print_log("event=\"%s\" time=%ld.%09ld\n", e, t->tv_sec, t->tv_nsec);
 }
 
 /**
  * Calculate difference and print event, times and difference.
  */
-void print_diff(const struct timespec *ct, const struct timespec *t, char *e)
+void print_diff(const struct timespec *current, const struct timespec *t, char *e)
 {
     struct timespec d;
 
-    if ((ct->tv_nsec - t->tv_nsec) < 0) {
-        d.tv_sec = ct->tv_sec - t->tv_sec - 1;
-        d.tv_nsec = ct->tv_nsec - t->tv_nsec + 1000000000;
+    if ((current->tv_nsec - t->tv_nsec) < 0) {
+        d.tv_sec = current->tv_sec - t->tv_sec - 1;
+        d.tv_nsec = current->tv_nsec - t->tv_nsec + 1000000000;
     } else {
-        d.tv_sec = ct->tv_sec - t->tv_sec;
-        d.tv_nsec = ct->tv_nsec - t->tv_nsec;
+        d.tv_sec = current->tv_sec - t->tv_sec;
+        d.tv_nsec = current->tv_nsec - t->tv_nsec;
     }
 
-    printf("event=\"%s\" start=%ld.%09ld stop=%ld.%09ld duration=%ld.%09ld\n", e, t->tv_sec, t->tv_nsec, ct->tv_sec, ct->tv_nsec, d.tv_sec, d.tv_nsec);
+    print_log(
+        "event=\"%s\" start=%ld.%09ld stop=%ld.%09ld duration=%ld.%09ld",
+        e,
+        t->tv_sec,
+        t->tv_nsec,
+        current->tv_sec,
+        current->tv_nsec,
+        d.tv_sec,
+        d.tv_nsec
+    );
+}
+
+int cmd_start(char *event) {
+    store_time(ct);
+    print_time(ct, event);
+
+    return 0;
+}
+
+int cmd_stop(char *event) {
+    struct timespec *t;
+
+    t = retrive_time();
+    if (t == NULL) {
+        t = get_current_time();
+        print_diff(t, ct, event);
+    } else {
+        print_diff(ct, t, event);
+    }
+
+
+    clear_storage();
+
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    struct timespec *ct = get_current_time();
-    struct timespec *t;
+    ct = get_current_time();
 
     if (argc < 2) {
         print_usage();
@@ -102,20 +155,9 @@ int main(int argc, char *argv[])
     }
 
     if (strcmp(argv[1], "start") == 0) {
-        store_time(ct);
-        print_time(ct, event);
-        exit(0);
+        return cmd_start(event);
     } else if (strcmp(argv[1], "stop") == 0) {
-        t = retrive_time();
-        clear_storage();
-
-        if (t == NULL) {
-            print_diff(get_current_time(), ct, event);
-        } else {
-            print_diff(ct, t, event);
-        }
-
-        exit(0);
+        return cmd_stop(event);
     }
 
     print_usage();
